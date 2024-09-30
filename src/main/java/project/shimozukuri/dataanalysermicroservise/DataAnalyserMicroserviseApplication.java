67762fb6232330568b218a1d744110768307dad8
shimozukuri <whitelord228@yandex.ru>
2024-10-01 00:15:14 +0300

    first commit

---
+++ src/main/java/project/shimozukuri/dataanalysermicroservise/DataAnalyserMicroserviseApplication.java
@@ -0,0 +1,13 @@
+package project.shimozukuri.dataanalysermicroservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyserMicroserviseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyserMicroserviseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/shimozukuri/dataanalysermicroservise/DataAnalyserMicroserviseApplicationTests.java
@@ -0,0 +1,13 @@
+package project.shimozukuri.dataanalysermicroservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalyserMicroserviseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
