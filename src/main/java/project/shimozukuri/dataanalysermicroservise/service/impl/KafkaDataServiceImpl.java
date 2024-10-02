32decfb98092fc463be135517c6323c5d51c769d
shimozukuri <whitelord228@yandex.ru>
2024-10-02 21:44:07 +0300

    add liquibase and postgres

--- src/main/java/project/shimozukuri/dataanalysermicroservise/service/impl/KafkaDataServiceImpl.java
+++ src/main/java/project/shimozukuri/dataanalysermicroservise/service/impl/KafkaDataServiceImpl.java
@@ -1,14 +1,23 @@
 package project.shimozukuri.dataanalysermicroservise.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import project.shimozukuri.dataanalysermicroservise.model.Data;
+import project.shimozukuri.dataanalysermicroservise.repository.DataRepository;
 import project.shimozukuri.dataanalysermicroservise.service.KafkaDataService;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class KafkaDataServiceImpl implements KafkaDataService {
+
+    private final DataRepository dataRepository;
 
     @Override
     public void handle(Data data) {
-        System.out.println("Data object is received: " + data.toString());
+        log.info("Data object {} was saved", data);
+
+        dataRepository.save(data);
     }
 }
